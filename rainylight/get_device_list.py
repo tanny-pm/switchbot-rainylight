96cfb542a1bb41cc95e74bba5ec9d126e9e1b3a7
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-02-26 15:14:51 +0900

    switchbot apiのコール方法を改善

--- rainylight/get_device_list.py
+++ rainylight/get_device_list.py
@@ -16,24 +16,25 @@ ACCESS_TOKEN: str = os.environ["SWITCHBOT_ACCESS_TOKEN"]
 SECRET: str = os.environ["SWITCHBOT_SECRET"]
 
 
-def generate_sign(token: str, secret: str, nonce: str = "") -> tuple[str, str, str]:
+def generate_sign(token: str, secret: str, nonce: str) -> tuple[str, str]:
     """SWITCH BOT APIの認証キーを生成する"""
 
     t = int(round(time.time() * 1000))
     string_to_sign = "{}{}{}".format(token, t, nonce)
-    string_to_sign = bytes(string_to_sign, "utf-8")
-    secret = bytes(secret, "utf-8")
+    string_to_sign_b = bytes(string_to_sign, "utf-8")
+    secret_b = bytes(secret, "utf-8")
     sign = base64.b64encode(
-        hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest()
+        hmac.new(secret_b, msg=string_to_sign_b, digestmod=hashlib.sha256).digest()
     )
 
-    return (str(t), str(sign, "utf-8"), nonce)
+    return (str(t), str(sign, "utf-8"))
 
 
 def get_device_list() -> str:
     """SWITCH BOTのデバイスリストを取得する"""
 
-    t, sign, nonce = generate_sign(ACCESS_TOKEN, SECRET)
+    nonce = "zzz"
+    t, sign = generate_sign(ACCESS_TOKEN, SECRET, nonce)
     headers = {
         "Authorization": ACCESS_TOKEN,
         "t": t,
